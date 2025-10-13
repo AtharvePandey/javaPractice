34a503c2b31beca0b9ecf39e1eadf5300c4ca284
Atharve Pandey <pandey.atharve@gmail.com>
2025-10-13 14:41:55 -0400

    added pathsum

--- src/App.java
+++ src/App.java
@@ -5130,4 +5130,23 @@ public class App {
         return 1 + Math.min(leftDepth, rightDepth);
     }
 
+    // Given the root of a binary tree and an integer targetSum, return true if
+    // the tree has a root-to-leaf path such that
+    // adding up all the values along the path equals targetSum.
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // we can either do recursive dfs or bfs to solve this
+        // lets do dfs with stack
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            // if we are at a leaf node, we gotta see if we can make target sum
+            return root.val == targetSum;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
 }
